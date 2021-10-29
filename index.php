122f9e1c3aa82f8a2ba5bdd98a5a6076917833fa
Tahnia20 <93360911+Tahnia20@users.noreply.github.com>
2021-10-29 13:47:20 +0700

    Ini index

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 Echo"Pengumpulan tugas pemrograman internet yang harus dikumpulkan di edlink jam 23.29 wib";
+Tahnia Gita Lestari Azizah C1983207024
+Ripa Saniatun C1983207004
+Belgis Nurhaliza C1983207017
+Pini Oktaviani C1983207022
 ?>
