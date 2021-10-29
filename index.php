6b08d24f867ee7a21c3fc56870f1e648bf338ce9
Tahnia20 <93360911+Tahnia20@users.noreply.github.com>
2021-10-29 13:37:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-Pengumpulan tugas pemrograman internet yang harus dikumpulkan di edlink jam 23.29 wib 
+<?php
+Echo"Pengumpulan tugas pemrograman internet yang harus dikumpulkan di edlink jam 23.29 wib";
+?>
