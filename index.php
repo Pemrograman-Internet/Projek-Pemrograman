9e1ccb67e2fa64e4fe073b1276fca2f9a412746a
belgisnurhaliza23 <93360548+belgisnurhaliza23@users.noreply.github.com>
2021-10-29 13:57:32 +0700

    index.php
    Belgis Nurhaliza C1983207022
    Pini Oktaviani C1983207017

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 Echo"Pengumpulan tugas pemrograman internet yang harus dikumpulkan di edlink jam 23.29 wib";
 Tahnia Gita Lestari Azizah C1983207024
 Ripa Saniatun C1983207004
-Belgis Nurhaliza C1983207017
-Pini Oktaviani C1983207022
+Belgis Nurhaliza C1983207022
+Pini Oktaviani C1983207017
 ?>
