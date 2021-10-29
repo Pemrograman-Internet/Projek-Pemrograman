fa2e5c1d1255e1250dcd0c9ddc284285240c78cd
Pini21 <93363397+Pini21@users.noreply.github.com>
2021-10-29 14:02:34 +0700

    ini.php
    Penambahan kalimat

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 Echo"Pengumpulan tugas pemrograman internet yang harus dikumpulkan di edlink jam 23.29 wib";
+Anggota kelompok : 
 Tahnia Gita Lestari Azizah C1983207024
 Ripa Saniatun C1983207004
 Belgis Nurhaliza C1983207022
